bb03fc908fe677d5828c75f1355a92c4178af777
starfys <stevensheffey4@gmail.com>
2020-07-11 04:00:53 -0500

    minimal printing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -188,39 +188,26 @@ async fn handle_connection(mut state: ServerState, raw_stream: TcpStream, addr: 
     println!("WebSocket connection established: {}", addr);
 
     // Insert the write part of this peer to the peer map.
-    let (tx, rx) = unbounded();
-    state.peer_map.write().unwrap().insert(addr, tx);
+    //let (tx, rx) = unbounded();
+    //state.peer_map.write().unwrap().insert(addr, tx);
 
     let (outgoing, incoming) = ws_stream.split();
 
-    let broadcast_incoming = incoming.try_for_each(|msg| {
+    let print_incoming = incoming.try_for_each(|msg| {
         info!(
             "Received a message from {}: {}",
             addr,
             msg.to_text().unwrap()
         );
-        let peers = state.peer_map.read().unwrap();
-
-        // We want to broadcast the message to everyone except ourselves.
-        let broadcast_recipients = peers
-            .iter()
-            .filter(|(peer_addr, _)| peer_addr != &&addr)
-            .map(|(_, ws_sink)| ws_sink);
-
-        for recp in broadcast_recipients {
-            recp.unbounded_send(msg.clone()).unwrap();
-        }
 
         future::ok(())
     });
 
-    let receive_from_others = rx.map(Ok).forward(outgoing);
-
-    pin_mut!(broadcast_incoming, receive_from_others);
-    future::select(broadcast_incoming, receive_from_others).await;
-
+    print_incoming
+        .await
+        .expect("Failed to handle incoming messages");
     info!("{} disconnected", &addr);
-    state.peer_map.as_ref().write().unwrap().remove(&addr);
+    //state.peer_map.as_ref().write().unwrap().remove(&addr);
 }
 
 #[tokio::main]
